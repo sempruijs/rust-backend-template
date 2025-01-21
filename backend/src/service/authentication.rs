24e6aaeed4959611063fafeaf3e0996caba5324c
sem pruijs <sem@pruijs.net>
2025-01-21 15:55:54 +0100

    Add comments and remove commented out functions

--- backend/src/service/authentication.rs
+++ backend/src/service/authentication.rs
@@ -9,21 +9,27 @@ use serde::Deserialize;
 use serde::Serialize;
 use std::str::FromStr;
 use uuid::Uuid;
-// use sqlx::PgPool;
 
+/// Claims are encoded in the JWT.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Claims {
     pub user_id: String,
     pub exp: usize, // Expiration time (as a timestamp)
 }
 
+/// business logic for authorisation.
 #[async_trait]
 pub trait AuthenticationService: Send + Sync {
+    /// Returns a JWT when credentials are valid.
     async fn login(&self, email: String, password: String) -> Result<Option<String>, sqlx::Error>;
 
+    /// Checks if a JWT is valid given credentials.
     async fn verify_jwt(&self, token: &str) -> Result<Option<User>, sqlx::Error>;
 }
 
+/// AuthentcationServiceImpl requires:
+/// User repository for recieving information about users.
+/// The secret key for signing JWT's.
 pub struct AuthenticationServiceImpl<U: UserRepository> {
     user_repository: U,
     secret_key: String,
@@ -38,6 +44,7 @@ impl<U: UserRepository> AuthenticationServiceImpl<U> {
     }
 }
 
+// Implement the authentication service trait for AuthenticationServiceImpl.
 #[async_trait]
 impl<U: UserRepository> AuthenticationService for AuthenticationServiceImpl<U> {
     async fn login(&self, email: String, password: String) -> Result<Option<String>, sqlx::Error> {
@@ -71,8 +78,6 @@ impl<U: UserRepository> AuthenticationService for AuthenticationServiceImpl<U> {
     }
 }
 
-// todo, should recieve now as argument.
-// IMPURE function.
 fn generate_jwt(user_id: Uuid, secret_key: String) -> Result<Option<String>, sqlx::Error> {
     // calculate experation time.
     let expiration = Utc::now()
@@ -93,66 +98,10 @@ fn generate_jwt(user_id: Uuid, secret_key: String) -> Result<Option<String>, sql
     )
     .expect("JWT creation failed");
 
-    return Ok(Some(token));
+    Ok(Some(token))
 }
 
 pub fn verify_password(password: &str, hashed_password: &str) -> bool {
     // Verify the password against the hashed password
     verify(password, hashed_password).unwrap_or(false)
 }
-
-// pub async fn authenticate_user(
-//     pool: &PgPool,
-//     email: &str,
-//     password: &str,
-//     secret_key: String,
-// ) -> Result<Option<String>, sqlx::Error> {
-//     // Fetch the hashed password for the given email
-//     let hashed_password: Option<String> =
-//         sqlx::query_scalar!("SELECT password FROM users WHERE email = $1", email)
-//             .fetch_optional(pool)
-//             .await?;
-
-//     if let Some(hashed_password) = hashed_password {
-//         if verify_password(password, &hashed_password) {
-//             // credentials are valid so jwt must be generated.
-//             // calculate experation time.
-//             let expiration = Utc::now()
-//                 .checked_add_signed(Duration::hours(24))
-//                 .expect("Invalid time")
-//                 .timestamp() as usize;
-
-//             // recieve uuid
-//             let uuid = get_user_uuid_by_email(pool, email)
-//                 .await?
-//                 .expect("failed to unwrap uuid based on email.")
-//                 .to_string();
-
-//             let claims = Claims {
-//                 uuid: uuid,
-//                 exp: expiration,
-//             };
-
-//             // generate jwt
-//             let token = encode(
-//                 &Header::default(),
-//                 &claims,
-//                 &EncodingKey::from_secret(secret_key.as_bytes()),
-//             )
-//             .expect("JWT creation failed");
-
-//             return Ok(Some(token));
-//         }
-//     }
-
-//     Ok(None) // Invalid credentials
-// }
-
-// pub fn verify_jwt(token: &str, secret_key: String) -> Result<Claims, jsonwebtoken::errors::Error> {
-//     decode::<Claims>(
-//         token,
-//         &DecodingKey::from_secret(secret_key.as_bytes()),
-//         &Validation::default(),
-//     )
-//     .map(|data| data.claims)
-// }
